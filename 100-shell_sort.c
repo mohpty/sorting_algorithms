e275db09614b6ff4eaa06bf4c391151cf374b4d0
mg5714 <mistergddora2000@gmail.com>
2024-01-19 19:10:22 +0200

    fix

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -22,7 +22,7 @@ void shell_sort(int *array, size_t size)
 		for (i = gap; i < size; i++)
 		{
 			tmp = array[i];
-			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+			for (j = i; j > gap - 1 && array[j - gap] > tmp; j -= gap)
 			{
 				array[j] = array[j - gap];
 			}

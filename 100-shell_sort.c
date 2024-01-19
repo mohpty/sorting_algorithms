f713e009434e9390f14fbff1e4436316a429d177
mg5714 <mistergddora2000@gmail.com>
2024-01-19 19:22:22 +0200

    fix

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -14,8 +14,11 @@ void shell_sort(int *array, size_t size)
 
 	if (!array || !size)
 		return;
-	for (gap = 1; gap < size / 3; gap = gap * 3 + 1)
-		;
+
+	gap = 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
 	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)

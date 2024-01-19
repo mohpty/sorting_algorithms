2b73a57e49ecb37fd95456c5d9aa948c82568a49
mg5714 <mistergddora2000@gmail.com>
2024-01-19 15:42:46 +0200

    104

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -6,33 +6,27 @@
  * @l_size: left size
  * @r_size: right size
  */
-void merge(int *array, int *temp, size_t left, size_t mid, size_t right)
+void merge(int *array, int *temp, size_t left, size_t right)
 {
-	size_t i = left, j = mid + 1, k = left;
+	size_t i = left, j, mid, k;
 
-	print_array(array + left, mid - left + 1);
+	j = mid = (left + right) / 2;
+	printf("Merging...\n[left]: ");
+	print_array(array + left, mid - left);
+	printf("[right]: ");
+	print_array(array + mid, right - mid);
 
-	print_array(array + mid + 1, right - mid);
-
-	while (i <= mid && j <= right)
+	k = left;
+	while (k < right)
 	{
-		if (array[i] <= array[j])
-			temp[k++] = array[i++];
+		if (i < mid && (j >= right || array[i] <= array[j]))
+			temp[k] = array[i++];
 		else
-			temp[k++] = array[j++];
+			temp[k] = array[j++];
 	}
 
-	while (i <= mid)
-		temp[k++] = array[i++];
-
-	while (j <= right)
-		temp[k++] = array[j++];
-
-	for (i = left; i <= right; i++)
-		array[i] = temp[i];
-
-
-	print_array(array + left, right - left + 1);
+	printf("[Done]: ");
+	print_array(array + left, right - left);
 }
 
 
@@ -47,13 +41,12 @@ void merge_sort_recursive(int *array, int *temp, size_t left, size_t right)
 {
 	size_t mid;
 
-	if (left < right)
+	mid = (right + left) / 2;
+	if (right - left < 2)
 	{
-		mid = left + (right - left) / 2;
-
 		merge_sort_recursive(array, temp, left, mid);
-		merge_sort_recursive(array, temp, mid + 1, right);
-		merge(array, temp, left, mid, right);
+		merge_sort_recursive(array, temp, mid, right);
+		merge(array, temp, left, right);
 	}
 }
 
@@ -66,16 +59,16 @@ void merge_sort(int *array, size_t size)
 {
 	int *temp;
 
-	temp = malloc(size * sizeof(int));
 	if (!array || size < 2)
 		return;
 
+	temp = malloc(size * sizeof(int));
 
 	if (!temp)
 	{
 		return;
 	}
 
-	merge_sort_recursive(array, temp, 0, size - 1);
+	merge_sort_recursive(array, temp, 0, size);
 	free(temp);
 }

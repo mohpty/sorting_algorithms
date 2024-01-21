5108ac1d554837f455976b99b8f4767e8f1e77a5
mg5714 <mistergddora2000@gmail.com>
2024-01-21 17:06:23 +0200

    fix 103

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -9,16 +9,16 @@
  */
 int getMax(int *array, size_t size)
 {
-	int max;
-	size_t i;
+        int max;
+        size_t i;
 
-	max = array[0];
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > max)
-		max = array[i];
-	}
-	return (max);
+        max = array[0];
+        for (i = 1; i < size; i++)
+        {
+                if (array[i] > max)
+                max = array[i];
+        }
+        return (max);
 }
 
 /**
@@ -27,15 +27,11 @@ int getMax(int *array, size_t size)
  * @size: size of array
  * @exp: Current significant
  */
-void countSort(int *array, size_t size, int exp)
+void countingsort(int *array, int size, int exp)
 {
-	int *output;
-	int count[10] = {0};
-	size_t i;
+    int *output = malloc(size * sizeof(int));
+    int i, count[10] = {0};
 
-	output = malloc(size * sizeof(int));
-	if (!output)
-		return;
 
 	for (i = 0; i < size; i++)
 		count[(array[i] / exp) % 10]++;
@@ -43,7 +39,7 @@ void countSort(int *array, size_t size, int exp)
 	for (i = 1; i < 10; i++)
 		count[i] += count[i - 1];
 
-	for (i = size - 1; i < SIZE_MAX; i--)
+	for (i = size - 1; i >= 0; i--)
 	{
 		output[count[(array[i] / exp) % 10] - 1] = array[i];
 		count[(array[i] / exp) % 10]--;
@@ -52,7 +48,6 @@ void countSort(int *array, size_t size, int exp)
 	for (i = 0; i < size; i++)
 	{
 		array[i] = output[i];
-		print_array(array, size);
 	}
 
 	free(output);
@@ -65,15 +60,21 @@ void countSort(int *array, size_t size, int exp)
  */
 void radix_sort(int *array, size_t size)
 {
-	int max, exp;
+	int i, max, exp;
+
 
 	if (!array || size < 2)
 		return;
 
 	max = getMax(array, size);
-
 	for (exp = 1; max / exp > 0; exp *= 10)
 	{
-		countSort(array, size, exp);
+		countingsort(array, size, exp);
+
+		for (i = 0; (size_t)i < size; i++)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
 	}
 }

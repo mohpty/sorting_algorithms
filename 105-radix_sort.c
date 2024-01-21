58ea04fe117596f1eb48a83ed0ccfcc208e70819
mg5714 <mistergddora2000@gmail.com>
2024-01-21 17:08:45 +0200

    fix 103

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -2,26 +2,6 @@
 #include <stdint.h>
 
 /**
- * getMax - get the maximum value in thearray
- * @array: array to find max
- * @size: size of array
- * Return: max value.
- */
-int getMax(int *array, size_t size)
-{
-        int max;
-        size_t i;
-
-        max = array[0];
-        for (i = 1; i < size; i++)
-        {
-                if (array[i] > max)
-                max = array[i];
-        }
-        return (max);
-}
-
-/**
  * countSort - perform counting sort.
  * @array: array to be sorted
  * @size: size of array
@@ -62,11 +42,14 @@ void radix_sort(int *array, size_t size)
 {
 	int i, max, exp;
 
-
+	max = array[0];
 	if (!array || size < 2)
 		return;
 
-	max = getMax(array, size);
+	for (i = 1; (size_t)i < size; i++)
+                if (array[i] > max)
+                        max = array[i];
+
 	for (exp = 1; max / exp > 0; exp *= 10)
 	{
 		countingsort(array, size, exp);
